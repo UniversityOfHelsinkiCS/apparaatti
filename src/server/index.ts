7ea5d86178e08b9fe5af2eda0e75bbd5cc3f9612
Sinisalo Matias A <szmatias@lx2-500-168.ad.helsinki.fi>
2025-04-03 12:21:47 +0300

    scaffolding

---
+++ src/server/index.ts
@@ -0,0 +1,16 @@
+import express from 'express'
+
+const app = express()
+
+
+app.listen(process.env.PORT, () => {
+  console.log("Listening on port " + process.env.PORT)
+})
+
+
+app.get('/', (req, res) => {
+  res.send("hello")
+})
+
+
+
