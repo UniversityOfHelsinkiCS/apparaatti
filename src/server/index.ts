fce67b51f4966ab8408781d08b5f877bda655e7a
Sinisalo Matias A <szmatias@lx2-500-168.ad.helsinki.fi>
2025-04-04 13:35:45 +0300

    backend now serves the frontend

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,16 +1,28 @@
+import path from 'path'
+import { dirname } from 'node:path'
+import { fileURLToPath } from 'node:url'
+
 import express from 'express'
 
 const app = express()
 
-
-app.listen(process.env.PORT, () => {
-  console.log("Listening on port " + process.env.PORT)
+app.use('/api/ping', (req, res, next) => {res.send("pong")})
+app.use('/api', (_, res) => {
+  res.sendStatus(404)
 })
 
+if (process.env.NODE_ENV === "production") {
+  const DIST_PATH = path.resolve(
+    dirname(fileURLToPath(import.meta.url)),
+    '../../dist'
+  )
 
-app.get('/api', (req, res) => {
-  res.send("hello")
+  const INDEX_PATH = path.resolve(DIST_PATH, 'index.html')
+
+  app.use(express.static(DIST_PATH))
+  app.get('*', (_, res) => res.sendFile(INDEX_PATH))
+}
+
+app.listen(process.env.PORT, async () => {
+   console.log(`Server running on port ${process.env.PORT}`)
 })
-
-
-
