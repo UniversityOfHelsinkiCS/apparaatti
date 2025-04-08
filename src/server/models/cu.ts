5c35c44d04f0320002dc4497db868957ecb0a169
Veikkosuhonen <vesuvesuh@gmail.com>
2025-04-08 10:56:20 +0300

    fix extension

--- src/server/models/cu.ts
+++ src/server/models/cu.ts
@@ -1,5 +1,5 @@
 import { Model, DataTypes, CreationOptional, InferAttributes, InferCreationAttributes } from 'sequelize';
-import { sequelize } from '../db/connection';
+import { sequelize } from '../db/connection.ts';
 import { LocalizedString } from '../../common/types.ts';
 
 class Cu extends Model<
