949a8950c6625a4cf00f5caf280e414a1e51e85a
Veikkosuhonen <vesuvesuh@gmail.com>
2025-04-08 13:00:32 +0300

    fix front

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,49 +1,24 @@
-import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 import MultiChoiceForm from './components/MultiChoiceForm.tsx';
 import { useQuery } from '@tanstack/react-query';
 import { Form } from '../common/types.ts';
 
-
-
-
 function App() {
- 
-
-  async function callHome(){
-    const req = await fetch('/api')
-    const answer = await req.text()
-    console.log(answer)
-    return answer
-  }
-
-
-  const { data, isLoading, error } = useQuery({
+  const { data: form, isLoading } = useQuery<Form>({
     queryKey: ['form'],
-    queryFn: () => {
-      return fetch('/api/form/1')
-        .then((res) => res.json())
-        .then((data) => {
-          console.log(data)
-          return data
-        })
+    queryFn: async () => {
+      const res = await fetch('/api/form/1')
+      return res.json()
     },
   })
 
-  useEffect(() => {
-    callHome()
-  }, [])
-
-  if (isLoading) {
+  if (isLoading || !form) {
     return <div>Loading...</div>
   }
 
   return (
     <>
      
-      <MultiChoiceForm form={data}/>
+      <MultiChoiceForm form={form}/>
    
     </>
   )
