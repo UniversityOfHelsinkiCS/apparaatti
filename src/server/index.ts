75b424805829adf3a203754b3f413f358904e613
Sinisalo Matias A <szmatias@lx2-500-168.ad.helsinki.fi>
2025-04-11 14:23:21 +0300

    use oidc.ts from another project

--- src/server/index.ts
+++ src/server/index.ts
@@ -13,6 +13,8 @@ import { REDIS_URL, SESSION_SECRET, OIDC_AUTHORIZATION_URL, OIDC_ISSUER, OIDC_TO
 import {createClient} from 'redis'
 import {RedisStore} from 'connect-redis'
 import OpenIDConnectStrategy from 'passport-openidconnect';
+import { set } from 'zod'
+import setupAuthentication from './util/oidc.ts'
 
 
 const redisClient = createClient({ 
@@ -53,5 +55,6 @@ if (process.env.NODE_ENV === "production") {
 app.listen(process.env.PORT, async () => {
   await connectToDatabase()
   await seed()
+  await setupAuthentication()
   console.log(`Server running on port ${process.env.PORT}`)
 })
