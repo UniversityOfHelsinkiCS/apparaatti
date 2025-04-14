bb894fe3b392140d5f981a7b4df6fc28a7a8aafc
MatiasSinisalo <matias.aleksanteri@hotmail.fi>
2025-04-14 14:32:25 +0300

    debug token set

--- src/server/util/oidc.ts
+++ src/server/util/oidc.ts
@@ -58,7 +58,7 @@ const getClient = async () => {
 
 const verifyLogin = async (_tokenSet: openidClient.TokenSet, userinfo: openidClient.UserinfoResponse<openidClient.UnknownObject, openidClient.UnknownObject>, done: (err: any, user?: unknown) => void) => {
   console.log('User info:', userinfo)
- 
+  console.log('Token set:', _tokenSet)
  
   const { uid: username, hyPersonSisuId: id, given_name: firstName, family_name: lastName, schacDateOfBirth, email, hyGroupCn: iamGroups } = userinfo as unknown as OpenIDAttributes
 
