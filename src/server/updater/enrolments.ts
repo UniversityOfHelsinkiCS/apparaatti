38918a198b54239b2d229dafc919b60b9ae3c5f0
MatiasSinisalo <matias.aleksanteri@hotmail.fi>
2025-04-22 12:58:22 +0300

    remove unused code

--- src/server/updater/enrolments.ts
+++ src/server/updater/enrolments.ts
@@ -1,50 +1,12 @@
 import _ from 'lodash'
-import { Op } from 'sequelize'
-import { ChatInstance, Enrolment } from '../db/models'
+
 import { mangleData } from './mangleData'
-import { safeBulkCreate } from './util'
+
 
 interface SisuEnrolment {
   courseId: string
   courseUnitRealisationId: string
   personId: string
-}
-const enrolmentsHandler = async (enrolments: SisuEnrolment[]) => {
-  const enrolmentsMap = _.groupBy(enrolments, 'courseUnitRealisationId')
-
-  const relatedCourseIds = enrolments.map(
-    ({ courseUnitRealisationId }) => courseUnitRealisationId
-  )
-  const relatedChatInstances = await ChatInstance.findAll({
-    where: {
-      courseId: {
-        [Op.in]: relatedCourseIds,
-      },
-    },
-    attributes: ['id', 'courseId'],
-  })
-
-  const toInsert = relatedChatInstances.flatMap((chatInstance) =>
-    (enrolmentsMap[chatInstance.courseId] || []).map((enrolment) => ({
-      chatInstanceId: chatInstance.id,
-      userId: enrolment.personId,
-    }))
-  )
-
-  await safeBulkCreate({
-    entityName: 'Enrolment',
-    entities: toInsert,
-    bulkCreate: async (e, opt) => Enrolment.bulkCreate(e, opt),
-    fallbackCreate: async (e, opt) => Enrolment.upsert(e, opt),
-    bulkCreateOptions: {
-      conflictAttributes: ['userId', 'chatInstanceId'],
-      ignoreDuplicates: true,
-    },
-    fallbackCreateOptions: {
-      conflictFields: ['user_id', 'chat_instance_id'],
-      fields: ['user_id', 'chat_instance_id'],
-    },
-  })
 }
 
 export const fetchEnrolments = async () => {
