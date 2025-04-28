d21f742be44fa2f2c78d5499e3adf24e309a1e67
MatiasSinisalo <matias.aleksanteri@hotmail.fi>
2025-04-28 14:06:48 +0300

    move recommendation logic to util/recommender.ts

--- src/server/routes/router.ts
+++ src/server/routes/router.ts
@@ -6,6 +6,7 @@ import User from '../db/models/user.ts';
 import passport from 'passport';
 import type { CourseRecommendation } from '../../common/types.ts';
 import Form from '../db/models/form.ts';
+import recommendCourses from '../util/recommender.ts';
 
 
 const router = express.Router();
@@ -30,133 +31,6 @@ async function saveAnswer(answerData: any, user: User) {
   return answer;
 }
 
-function convertAnswerValueToFloat(answerValue){
-  switch (answerValue) {
-    case '1':
-      return 0.0;
-    case '2':
-      return 0.5;
-    case '3':
-      return 1.0;
-    default:
-      return 0.0;
-  }
-}
-
-
-function calculateUserCoordinates(answerData: any) {
-  console.log('Calculating user coordinates based on answer data:', answerData);
-  const userCoordinates = {
-    fear: convertAnswerValueToFloat(answerData['1']),
-    teachingMethod: convertAnswerValueToFloat(answerData['2']),
-    experience: convertAnswerValueToFloat(answerData['3']),
-  }
-  console.log('User coordinates:', userCoordinates);
-  return userCoordinates;
-}
-
-//calculates distance between user and course coordinates, assumes 3 dimensions
-//returns a list of [{course, distance}] 
-function calculateUserDistances(userCoordinates: any, availableCourses: CourseRecommendation[]) {
-  const distances = availableCourses.map(course => { 
-    const courseCoordinates = course.courseRecommendationCoordinates;
-    const distance = Math.sqrt(
-      Math.pow(userCoordinates.fear - courseCoordinates.fear, 2) +
-      Math.pow(userCoordinates.teachingMethod - courseCoordinates.teachingMethod, 2) +
-      Math.pow(userCoordinates.experience - courseCoordinates.experience, 2)
-    );
-    return { ...course, distance };
-  })
-
-  return distances
-}
-
-function getRecommendations(userCoordinates){
-  // Mock recommendation logic
-  const availableCourses: CourseRecommendation[] = [
-    {
-      id: '1',
-      name: 'Course 1',
-      description: 'Description 1',
-      courseCode: 'CS101',
-      credits: 5,
-      url: 'https://example.com/course1',
-      courseRecommendationCoordinates: {
-        fear: 0.5,
-        teachingMethod: 0.5,
-        experience: 0.5,
-      }
-    },
-    {
-      id: '2',
-      name: 'Course 2',
-      description: 'Description 2',
-      courseCode: 'CS102',
-      credits: 5,
-      url: 'https://example.com/course2',
-      courseRecommendationCoordinates: {
-        fear: 1.0,
-        teachingMethod: 1.0,
-        experience: 0,
-      }
-    },
-    {
-      id: '3',
-      name: 'Course 3',
-      description: 'Description 3',
-      courseCode: 'CS103',
-      credits: 5,
-      url: 'https://example.com/course3',
-      courseRecommendationCoordinates: {
-        fear: 0.0,
-        teachingMethod: 0,
-        experience: 1.0,
-      }
-    },
-    {
-      id: '4',
-      name: 'Course 4',
-      description: 'Description 4',
-      courseCode: 'CS103',
-      credits: 5,
-      url: 'https://example.com/course3',
-      courseRecommendationCoordinates: {
-        fear: 0.0,
-        teachingMethod: 0.0,
-        experience: 0.0,
-      }
-    },
-    {
-      id: '5',
-      name: 'Course 5',
-      description: 'Description 5',
-      courseCode: 'CS103',
-      credits: 5,
-      url: 'https://example.com/course3',
-      courseRecommendationCoordinates: {
-        fear: 1.0,
-        teachingMethod: 1.0,
-        experience: 1.0,
-      }
-    }
-  ]
-
-  const distances = calculateUserDistances(userCoordinates, availableCourses);
-  const sortedCourses = distances.sort((a, b) => a.distance - b.distance);
-  const recommendations = sortedCourses.slice(0, 3).map(course => ({
-    id: course.id,
-    name: course.name,
-    description: course.description,
-    courseCode: course.courseCode,
-    credits: course.credits,
-    url: course.url,
-    courseRecommendationCoordinates: course.courseRecommendationCoordinates,
-  }));
-
-  
-  return recommendations;
-}
-
 router.post('/form/1/answer', async (req, res) => {
   const answerData = AnswerSchema.parse(req.body);
 
@@ -169,11 +43,7 @@ router.post('/form/1/answer', async (req, res) => {
   }
 
   await saveAnswer(answerData, user)
-
-  const userCoordinates = calculateUserCoordinates(answerData);
-  const recommendations = getRecommendations(userCoordinates);
-
-
+  const recommendations = recommendCourses(answerData);
   res.json(recommendations);
 })
 
