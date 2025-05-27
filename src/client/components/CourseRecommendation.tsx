2c1d0d985fd47a78e29c9288d25b99297b8de570
MatiasSinisalo <matias.aleksanteri@hotmail.fi>
2025-05-27 15:51:32 +0300

    add visit link

--- src/client/components/CourseRecommendation.tsx
+++ src/client/components/CourseRecommendation.tsx
@@ -3,12 +3,16 @@ import { Box, Paper, Typography } from '@mui/material'
 
 const CourseRecommendation = ({ course }: { course: CourseRecommendationType }) => {
   if (!course) return null
-
+  const baseUrl = 'https://studies.helsinki.fi/kurssit/toteutus'
+  const courseUrl = `${baseUrl}/${course.course.id}`
   return (
     <Paper elevation={2} sx={{ padding: 2, margin: 1 }}>
       <Box>
         <Typography variant="h5" component="h2" gutterBottom>
           {course.course.name.fi}
+        </Typography>
+        <Typography variant="body1" gutterBottom>
+          <a href={courseUrl}>visit</a>
         </Typography>
        </Box>
     </Paper>
