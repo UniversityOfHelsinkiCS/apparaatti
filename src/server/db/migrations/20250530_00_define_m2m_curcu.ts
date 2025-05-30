e9a398c674775613653787c25a4d581d490b33c1
MatiasSinisalo <matias.aleksanteri@hotmail.fi>
2025-05-30 14:31:27 +0300

    define migration

---
+++ src/server/db/migrations/20250530_00_define_m2m_curcu.ts
@@ -0,0 +1,31 @@
+import { DataTypes, JSONB } from 'sequelize'
+
+import { sequelize, type Migration } from '../connection.ts'
+import Cur from '../models/cur.ts'
+import Cu from '../models/cu.ts'
+import CurCu from '../models/curCu.ts'
+
+export const up: Migration = async ({ context: queryInterface }) => {
+  
+  queryInterface.addColumn('CurCu', 'CurId', {
+    type: DataTypes.STRING,
+    references: {
+      model: Cur,
+      key: 'id'
+    }
+  })
+  queryInterface.addColumn('CurCu', 'CuId', {
+    type: DataTypes.STRING,
+    references: {
+      model: Cu,
+      key: 'id'
+    }
+  })
+  Cur.belongsToMany(Cu, {through: 'CurCu'})
+  Cu.belongsToMany(Cur, {through: 'CurCu'})
+}
+
+export const down: Migration = async ({ context: queryInterface }) => {
+  await queryInterface.removeColumn('CurCu', 'CurId')
+  await queryInterface.removeColumn('CurCu', 'CuId')
+}
