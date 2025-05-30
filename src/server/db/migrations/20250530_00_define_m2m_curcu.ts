3b3a5774ba2ecd6a39685b408741386af788ddcd
MatiasSinisalo <matias.aleksanteri@hotmail.fi>
2025-05-30 14:52:37 +0300

    fix

--- src/server/db/migrations/20250530_00_define_m2m_curcu.ts
+++ src/server/db/migrations/20250530_00_define_m2m_curcu.ts
@@ -5,28 +5,10 @@ import Cur from '../models/cur.ts'
 import Cu from '../models/cu.ts'
 import CurCu from '../models/curCu.ts'
 
-export const up: Migration = async ({ context: queryInterface }) => {
-  
-  await queryInterface.addColumn('CurCu', 'CurId', {
-    type: DataTypes.STRING,
-    references: {
-      model: Cur,
-      key: 'id'
-    }
-  })
-  
-  await queryInterface.addColumn('CurCu', 'CuId', {
-    type: DataTypes.STRING,
-    references: {
-      model: Cu,
-      key: 'id'
-    }
-  })
+export const up: Migration = async ({ context: queryInterface }) => {  
   Cur.belongsToMany(Cu, {through: CurCu})
   Cu.belongsToMany(Cur, {through: CurCu})
 }
 
 export const down: Migration = async ({ context: queryInterface }) => {
-  await queryInterface.removeColumn('CurCu', 'CurId')
-  await queryInterface.removeColumn('CurCu', 'CuId')
 }
