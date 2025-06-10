2f127b44969d83ae9560dcbf8bd0f3a08b230d11
MatiasSinisalo <matias.aleksanteri@hotmail.fi>
2025-06-10 14:41:01 +0300

    fix

--- src/server/db/migrations/20250610_00_add_education_phase_column.ts
+++ src/server/db/migrations/20250610_00_add_education_phase_column.ts
@@ -6,11 +6,11 @@ import Cu from '../models/cu.ts'
 import CurCu from '../models/curCu.ts'
 
 export const up: Migration = async ({ context: queryInterface }) => {  
-  await queryInterface.addColumn('studyRight', 'educationPhase1', {type: DataTypes.JSONB, allowNull: true })
-  await queryInterface.addColumn('studyRight', 'educationPhase2', {type: DataTypes.JSONB, allowNull: true })
+  await queryInterface.addColumn('studyrights', 'educationPhase1', {type: DataTypes.JSONB, allowNull: true })
+  await queryInterface.addColumn('studyrights', 'educationPhase2', {type: DataTypes.JSONB, allowNull: true })
 }
 
 export const down: Migration = async ({ context: queryInterface }) => {
-  await queryInterface.removeColumn('studyRight', 'educationPhase1')
-  await queryInterface.removeColumn('studyRight', 'educationPhase2')
+  await queryInterface.removeColumn('studyrights', 'educationPhase1')
+  await queryInterface.removeColumn('studyrights', 'educationPhase2')
 }
