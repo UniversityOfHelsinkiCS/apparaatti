9857333cc607cf2547d75420a56e87290e40750a
MatiasSinisalo <matias.aleksanteri@hotmail.fi>
2025-06-11 13:13:38 +0300

    made studyphasequestion work with the rest of the form

--- src/client/components/StudyPhaseQuestion.tsx
+++ src/client/components/StudyPhaseQuestion.tsx
@@ -18,22 +18,23 @@ const StudyPhaseQuestion = ({ studyData }: { studyData: any }) => {
 
   return (
     <Box sx={{ minWidth: 200, marginBottom: 2 }}>
-      <FormControl fullWidth sx={{ marginBottom: 2 }}>
-        <Typography id="study-phase-select-label">Select Phase</Typography>
-        <Select
-          labelId="study-phase-select-label"
-          value={selectedPhase}
-          onChange={handlePhaseChange}
-        >
-          <MenuItem value="phase1">1. vaiheen opinnot</MenuItem>
-          <MenuItem value="phase2">2. vaiheen opinnot</MenuItem>
-        </Select>
-      </FormControl>
-
+    
+      <Typography id="study-phase-select-label">Select Phase</Typography>
+      <Select
+        labelId="study-phase-select-label"
+        name='study-phase-select'
+        value={selectedPhase}
+        onChange={handlePhaseChange}
+      >
+        <MenuItem value="phase1">1. vaiheen opinnot</MenuItem>
+        <MenuItem value="phase2">2. vaiheen opinnot</MenuItem>
+      </Select>
+    
       {selectedPhase && (
-        <FormControl fullWidth>
+        <>
           <Typography id="study-field-select-label">Valitse suunta</Typography>
           <Select
+            name='study-field-select'
             labelId="study-field-select-label"
             value={selectedField}
             onChange={handleFieldChange}
@@ -44,7 +45,7 @@ const StudyPhaseQuestion = ({ studyData }: { studyData: any }) => {
               </MenuItem>
             ))}
           </Select>
-        </FormControl>
+        </>
       )}
     </Box>
   )
