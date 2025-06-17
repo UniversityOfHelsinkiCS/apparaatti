3260e720a078d8abbb999305e717745ca4110034
matiassinisalo <matias.aleksanteri@hotmail.fi>
2025-06-17 12:10:25 +0300

    more cleaning

--- src/server/util/recommender.ts
+++ src/server/util/recommender.ts
@@ -5,7 +5,7 @@ import Cur from '../db/models/cur.ts'
 import CurCu from '../db/models/curCu.ts'
 import { readCodeData, readCsvData } from './dataImport.ts'
 import _ from 'lodash'
-import { closestPeriod, dateObjToPeriod, getStudyPeriod, parseDate } from './studyPeriods.ts'
+import { getStudyPeriod, parseDate } from './studyPeriods.ts'
 
 function recommendCourses(answerData: any, user) {
   const userCoordinates = calculateUserCoordinates(answerData)
@@ -14,23 +14,6 @@ function recommendCourses(answerData: any, user) {
   
   return recommendations
 }
-
-
-function getClosestPeriodFromUserPick(answerValue){
-  switch (answerValue) {
-  case '1':
-    return closestPeriod('period_1').period
-  case '2':
-    return closestPeriod('period_2').period
-  case '3':
-    return closestPeriod('period_3').period
-  case '4':
-    return  closestPeriod('period_4').period
-  default:
-    return  closestPeriod().period
-  }
-}
-
 
 function calculateUserCoordinates(answerData: any) {
   const pickedPeriod = getStudyPeriod('2025', answerData['study-period'])
@@ -42,22 +25,6 @@ function calculateUserCoordinates(answerData: any) {
   }
 
   return userCoordinates
-}
-
-async function getCodesForCur(course: Cur){
-  const curcus: CurCu[] = await CurCu.findAll({
-    where: {curId: course.id},
-  })
-
-  const cuIds = curcus.map((curcu) => curcu.cuId)
-
-  const cus: Cu[] = await Cu.findAll({
-    where: {id: cuIds}
-  })
-
-  const codes = cus.map(cu => cu.courseCode)
-
-  return codes
 }
 
 
@@ -76,7 +43,6 @@ function langCoordFromCode (code: string){
 }
 
 async function calculateCourseDistance(course: Cur, userCoordinates: any){
-  const period = coursePeriod(course)
   const dimensions = Object.keys(userCoordinates)
   // using random values for now...
   const courseCoordinates = {
@@ -99,28 +65,6 @@ async function calculateCourseDistance(course: Cur, userCoordinates: any){
   return {course: course, distance: distance }
 }
 
-
-function coursePeriod(course: Cur){
-  const periods = dateObjToPeriod(course.startDate)
- 
-  if(periods.length == 0){
-    console.log('!! no period found for course: ', course)
-    return 0.0 // 0.0 is the value for courses that somehow didnt fit any period
-  }
-
-
-  const periodDistances =  periods.map((period) => {
-    return {
-      period: period,
-      distance: course.startDate.getTime() - parseDate(period.start_date).getTime()
-    }
-  }).filter((p) => p.distance > 0)
-    .sort((a, b) => a.distance - b.distance)
-  
-  //technically course can be in multiple periods but will use the first one returned for now...
-  const period = periodDistances[0].period
-  return period
-}
 
 
 
@@ -191,8 +135,6 @@ async function getRealisationsWithCourseUnitCodes(courseCodeStrings: string[]) {
 async function getRecommendations(userCoordinates: any, answerData, user) {  
   console.log(userCoordinates)
   const startBench = Date.now()
- 
-  const year = new Date().getFullYear()
   
   const pickedPeriod = getStudyPeriod('2025', answerData['study-period'])
   
