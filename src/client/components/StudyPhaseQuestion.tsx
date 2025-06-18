d442d5d9465dfff1a295e1cbc3c1133b45176908
ViLP357 <vilipi2009@gmail.com>
2025-06-18 13:20:43 +0300

    no selection if only one field

--- src/client/components/StudyPhaseQuestion.tsx
+++ src/client/components/StudyPhaseQuestion.tsx
@@ -4,11 +4,6 @@ import { Box, MenuItem, Select, Typography } from '@mui/material'
 const StudyPhaseQuestion = ({ studyData }: { studyData: any }) => {
   const [selectedPhase, setSelectedPhase] = useState('')
   const [selectedField, setSelectedField] = useState('')
-
-  //const handlePhaseChange = (event: any) => {
-  //  setSelectedPhase(event.target.value)
-  //  setSelectedField('')
-  //}
   
   const handleFieldChange = (event: any ) => {
     setSelectedField(event.target.value)
@@ -23,56 +18,29 @@ const StudyPhaseQuestion = ({ studyData }: { studyData: any }) => {
     return ''
   }
 
-  //const getFirstFieldSelection = (initialPhase) => {
-  //  if (initialPhase === 'phase1' && studyData?.phase1Data?.length) {
-  //    return studyData.phase1Data[0].id
-  //  } else if (initialPhase === 'phase2' && studyData?.phase2Data?.length) {
-  //    return studyData.phase2Data[0].id
-  //  }
-  //  return ''
-  //}
-
-
-
-  //const currentData = selectedPhase === 'phase1' ? studyData?.phase1Data : studyData?.phase2Data
   const currentData = studyData?.phase1Data.concat(studyData?.phase2Data)
-  //console.log(currentData.length)
 
   useEffect(() => {
     const initialPhase = getFirstSelection()
-    //const initialField = getFirstFieldSelection(initialPhase)
     if (initialPhase) {
       setSelectedPhase(initialPhase)
 
-      //setSelectedField(initialField)
     }
-
-        
-   
-    //console.log("1 ehto")
     if (studyData.phase1Data != null) {
-      //console.log("phase1")
       setSelectedField(studyData.phase1Data[0].id)
     } else if (studyData.phase2Data) {
-      //console.log("phase2")
       setSelectedField(studyData.phase2Data[0].id)
     }
     
   }, [studyData])
 
 
-  //console.log(currentData)
-  //console.log(currentData.length)
-
   return (
     <Box sx={{ minWidth: 200, marginBottom: 2 }}>
-      
-  
-    
       {selectedPhase && (
         <>
           <Typography id="study-field-select-label">Valitse opintojen suunta</Typography>
-
+          {currentData.length > 1 && (
           <Select
             sx={{ padding: '1px', minWidth: 100, border: '1px solid lightgray' }}
             name='study-field-select'
@@ -82,28 +50,17 @@ const StudyPhaseQuestion = ({ studyData }: { studyData: any }) => {
           >
             {currentData?.map((item: any) => (
               <MenuItem key={item.id} value={item.id}>
-                {item.name.fi}
+                {item.name.fi} 
               </MenuItem>
             ))}
           </Select>
-          
+          )}
+          {currentData.length < 2 && (
+            <p style={{border: "2px solid lightgrey", padding:"10px", borderRadius: "5px", minWidth: "100px"}}>{currentData[0].name.fi}</p>
+          )}
         </>
       )}
     </Box>
   )
 }
-
 export default StudyPhaseQuestion
-
-
-//<Typography id="study-phase-select-label">Valitse opintojen vaihe:</Typography>
-//<Select
-//  sx={{ padding: '1px', minWidth: 100, border: '1px solid lightgray' }}
-//  labelId="study-phase-select-label"
-//  name='study-phase-select'
-//  value={selectedPhase}
-//  onChange={handlePhaseChange}
-//>
-//  <MenuItem value="phase1">1. vaiheen opinnot</MenuItem>
-//  <MenuItem value="phase2">2. vaiheen opinnot</MenuItem>
-//</Select>
