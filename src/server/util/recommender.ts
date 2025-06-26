a9f5b28e136f474c6aab5d1032ce51ff3f563d36
matiassinisalo <matias.aleksanteri@hotmail.fi>
2025-06-26 17:23:19 +0300

    tweak

--- src/server/util/recommender.ts
+++ src/server/util/recommender.ts
@@ -85,8 +85,8 @@ async function calculateCourseDistance(course: any, userCoordinates: any, studyD
   const courseCoordinates = {
     //'period': coursePeriodValue(period),
     date: course.startDate.getTime(),  
-    org: sameOrganisationAsUser === true ? 0 : Math.pow(10, 24), // the user has coordinate of 0 in the org dimension, we want to prioritise courses that have the same organisation as the users...
-    lang: correctLang === true ? 0 : Math.pow(10, 12)
+    org: sameOrganisationAsUser === true ? 0 : Math.pow(10, 12), // the user has coordinate of 0 in the org dimension, we want to prioritise courses that have the same organisation as the users...
+    lang: correctLang === true ? 0 : Math.pow(10, 24) // if the course is different language than the users pick we want to have it very far away
   }
   
   
