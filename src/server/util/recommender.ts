371b10f179f4710bbe0ebb95d687d40ad7ebe5ff
matiassinisalo <matias.aleksanteri@hotmail.fi>
2025-06-30 15:38:08 +0300

    trying different typing

--- src/server/util/recommender.ts
+++ src/server/util/recommender.ts
@@ -79,6 +79,11 @@ function courseHasCustomCodeUrn(course: CourseRealization, codeUrn: string){
   console.log(course)
   const customCodeUrns = course.customCodeUrns
   console.log(customCodeUrns)
+   
+
+  console.log(Object.keys(customCodeUrns))
+  console.log(customCodeUrns.keys())
+
 
   for(const key of Object.keys(customCodeUrns)){
     if(key.includes('kk-apparaatti')){
