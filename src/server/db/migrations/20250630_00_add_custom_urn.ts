51af21d6908bde8654f18c880b761b89becdbf6b
matiassinisalo <matias.aleksanteri@hotmail.fi>
2025-07-04 14:00:31 +0300

    a small crime: comment away some migration code

--- src/server/db/migrations/20250630_00_add_custom_urn.ts
+++ src/server/db/migrations/20250630_00_add_custom_urn.ts
@@ -2,15 +2,17 @@ import { DataTypes } from 'sequelize'
 
 import { type Migration } from '../connection.ts'
 
+//this migration was renamed so to prevent this from running again it is commented away
+
 export const up: Migration = async ({ context: queryInterface }) => {
-  await queryInterface.addColumn('curs','custom_code_urns', {
-    type: DataTypes.JSONB,
-    allowNull: true,
-  })
+//  await queryInterface.addColumn('curs','custom_code_urns', {
+//    type: DataTypes.JSONB,
+//    allowNull: true,
+//  })
  
 }
 
 export const down: Migration = async ({ context: queryInterface }) => {
-  await queryInterface.removeColumn('curs', 'custom_code_urns')
+//  await queryInterface.removeColumn('curs', 'custom_code_urns')
 }
 
