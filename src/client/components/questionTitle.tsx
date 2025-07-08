9de0fb9f3915be4c5ef824a0b71a6b1affda6be6
matiassinisalo <matias.aleksanteri@hotmail.fi>
2025-07-08 13:19:30 +0300

    adjusted padding

--- src/client/components/questionTitle.tsx
+++ src/client/components/questionTitle.tsx
@@ -6,7 +6,7 @@ import InfoIcon from '@mui/icons-material/Info'
 
 const QuestionTitle = ({handleOpen, title}: {handleOpen: () => void, title: string | undefined}) => {
   return (
-    <Stack direction='row' sx={{display: 'flex', borderTop: '2px solid gray'}}>
+    <Stack direction='row' sx={{display: 'flex', borderTop: '2px solid gray', marginTop: '5rem'}}>
       <Typography gutterBottom sx={{ fontSize: '1rem', width: 'auto' }}>
         <IconButton onClick={handleOpen} aria-label='more info' sx={{paddingLeft: 0}}>
           <InfoIcon></InfoIcon> 
