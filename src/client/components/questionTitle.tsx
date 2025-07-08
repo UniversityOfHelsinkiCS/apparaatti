7a60efc3c1fe91700201e121102454ffeff616f5
matiassinisalo <matias.aleksanteri@hotmail.fi>
2025-07-08 12:57:35 +0300

    added border

--- src/client/components/questionTitle.tsx
+++ src/client/components/questionTitle.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, IconButton, Stack, Typography } from '@mui/material'
+import { IconButton, Stack, Typography } from '@mui/material'
 import InfoIcon from '@mui/icons-material/Info'
 
 
@@ -6,7 +6,7 @@ import InfoIcon from '@mui/icons-material/Info'
 
 const QuestionTitle = ({handleOpen, title}: {handleOpen: () => void, title: string | undefined}) => {
   return (
-    <Stack direction='row' sx={{display: 'flex'}}>
+    <Stack direction='row' sx={{display: 'flex', borderTop: '2px solid gray'}}>
       <Typography gutterBottom sx={{ fontSize: '1rem', width: 'auto' }}>
         <IconButton onClick={handleOpen} aria-label='more info' sx={{paddingLeft: 0}}>
           <InfoIcon></InfoIcon> 
