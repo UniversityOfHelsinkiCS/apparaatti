ac45a4f69a86ab2708369202e327abbebbf24fe3
matiassinisalo <matias.aleksanteri@hotmail.fi>
2025-07-08 12:51:30 +0300

    got the icon to have the correct spacing in the title

--- src/client/components/questionTitle.tsx
+++ src/client/components/questionTitle.tsx
@@ -1,4 +1,4 @@
-import { Button, Typography } from '@mui/material'
+import { Box, Button, IconButton, Stack, Typography } from '@mui/material'
 import InfoIcon from '@mui/icons-material/Info'
 
 
@@ -6,12 +6,14 @@ import InfoIcon from '@mui/icons-material/Info'
 
 const QuestionTitle = ({handleOpen, title}: {handleOpen: () => void, title: string | undefined}) => {
   return (
-    <Typography gutterBottom sx={{ fontSize: '1rem', width: 'auto' }}>
-      <Button onClick={handleOpen} style={{ color: 'black' }}>
-        <InfoIcon></InfoIcon>
-      </Button>
-      {title}
-    </Typography>
+    <Stack direction='row' sx={{display: 'flex'}}>
+      <Typography gutterBottom sx={{ fontSize: '1rem', width: 'auto' }}>
+        <IconButton onClick={handleOpen} aria-label='more info' sx={{paddingLeft: 0}}>
+          <InfoIcon></InfoIcon> 
+        </IconButton>
+        {title}
+      </Typography>
+    </Stack>
   )  
 }
 
