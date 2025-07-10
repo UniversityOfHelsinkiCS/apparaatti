6c8a25e6c25a231a2a2705c1737bfaf80c597206
matiassinisalo <matias.aleksanteri@hotmail.fi>
2025-07-10 16:00:02 +0300

    changed the way studyrights are fetched

--- src/server/updater/studyRights.ts
+++ src/server/updater/studyRights.ts
@@ -2,8 +2,10 @@ import StudyRight from '../db/models/studyRight.ts'
 import { mangleData } from './mangleData.ts'
 
 const studyRightsHandler = async (studyRights: any[]) => {
-  const creations = studyRights.map((studyRight: any) => {
-    return StudyRight.upsert({
+  console.log('starting to save studyrights')
+  let count = 0
+  for(const studyRight of studyRights ){
+    await StudyRight.upsert({
       id: studyRight.id,
       personId: studyRight.personId,
       state: studyRight.state,
@@ -32,8 +34,43 @@ const studyRightsHandler = async (studyRights: any[]) => {
       createdAt: studyRight.createdAt,
       updatedAt: studyRight.updatedAt,
     })
-  })
-  await Promise.all(creations)
+    count++
+  }
+  console.log('finished batch of: ', count)
+    
+  
+  // const creations = studyRights.map((studyRight: any) => {
+  //   return StudyRight.upsert({
+  //     id: studyRight.id,
+  //     personId: studyRight.personId,
+  //     state: studyRight.state,
+  //     educationId: studyRight.educationId,
+  //     organisationId: studyRight.organisationId,
+  //     modificationOrdinal: studyRight.modificationOrdinal,
+  //     documentState: studyRight.documentState,
+  //     valid: studyRight.valid,
+  //     grantDate: studyRight.grantDate,
+  //     studyStartDate: studyRight.studyStartDate,
+  //     transferOutDate: studyRight.transferOutDate,
+  //     termRegistrations: studyRight.termRegistrations,
+  //     studyRightCancellation: studyRight.studyRightCancellation,
+  //     studyRightGraduation: studyRight.studyRightGraduation,
+  //     snapshotDateTime: studyRight.snapshotDateTime,
+  //     acceptedSelectionPath: studyRight.acceptedSelectionPath,
+  //     studyRightTransfer: studyRight.studyRightTransfer,
+  //     studyRightExtensions: studyRight.studyRightExtensions,
+  //     transferOutUniversityUrn: studyRight.transferOutUniversityUrn,
+  //     requestedSelectionPath: studyRight.requestedSelectionPath,
+  //     phase1MinorSelection: studyRight.phase1MinorSelection,
+  //     phase2MinorSelection: studyRight.phase2MinorSelection,
+  //     admissionTypeUrn: studyRight.admissionTypeUrn,
+  //     educationPhase1: studyRight.educationPhase1,
+  //     educationPhase2: studyRight.educationPhase2,
+  //     createdAt: studyRight.createdAt,
+  //     updatedAt: studyRight.updatedAt,
+  //   })
+  // })
+  // await Promise.all(creations)
 }
 
 //assumes that there is a table Users with studentNumber field
