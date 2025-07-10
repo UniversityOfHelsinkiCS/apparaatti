601f641bc63518bf20601ce3ad6940c36b14da2b
matiassinisalo <matias.aleksanteri@hotmail.fi>
2025-07-10 11:47:11 +0300

    using scrollIntoView

--- src/client/components/CourseRecommendationsPage.tsx
+++ src/client/components/CourseRecommendationsPage.tsx
@@ -2,11 +2,17 @@ import { Box, Stack } from '@mui/material'
 import CourseRecommendation from './CourseRecommendation'
 import type { CourseRecommendation as CourseRecommendationType } from '../../common/types'
 import ActionButton from './actionButton'
+import { useEffect, useRef } from 'react'
 
 const CourseRecommendationsPage = ({onClose, recommendations}: {onClose: () => void, recommendations: CourseRecommendationType[]}) => {
+  const topOfPage = useRef<HTMLAnchorElement | null>(null)
+  useEffect(() => {
+    topOfPage.current?.scrollIntoView()
+  }, [])
   return (
     <Box>
       <Stack>
+        <a ref={topOfPage} style={{display: 'none'}}></a>
         <ActionButton onClick={onClose} text="Takaisin"/>
         <Stack
           spacing={2}
