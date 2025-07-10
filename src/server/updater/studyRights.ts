ae5d25e263d82cc0a143b3847283b71ca32e2eb9
matiassinisalo <matias.aleksanteri@hotmail.fi>
2025-07-10 16:09:09 +0300

    remove commented code

--- src/server/updater/studyRights.ts
+++ src/server/updater/studyRights.ts
@@ -36,41 +36,7 @@ const studyRightsHandler = async (studyRights: any[]) => {
     })
     count++
   }
-  console.log('finished batch of: ', count)
-    
-  
-  // const creations = studyRights.map((studyRight: any) => {
-  //   return StudyRight.upsert({
-  //     id: studyRight.id,
-  //     personId: studyRight.personId,
-  //     state: studyRight.state,
-  //     educationId: studyRight.educationId,
-  //     organisationId: studyRight.organisationId,
-  //     modificationOrdinal: studyRight.modificationOrdinal,
-  //     documentState: studyRight.documentState,
-  //     valid: studyRight.valid,
-  //     grantDate: studyRight.grantDate,
-  //     studyStartDate: studyRight.studyStartDate,
-  //     transferOutDate: studyRight.transferOutDate,
-  //     termRegistrations: studyRight.termRegistrations,
-  //     studyRightCancellation: studyRight.studyRightCancellation,
-  //     studyRightGraduation: studyRight.studyRightGraduation,
-  //     snapshotDateTime: studyRight.snapshotDateTime,
-  //     acceptedSelectionPath: studyRight.acceptedSelectionPath,
-  //     studyRightTransfer: studyRight.studyRightTransfer,
-  //     studyRightExtensions: studyRight.studyRightExtensions,
-  //     transferOutUniversityUrn: studyRight.transferOutUniversityUrn,
-  //     requestedSelectionPath: studyRight.requestedSelectionPath,
-  //     phase1MinorSelection: studyRight.phase1MinorSelection,
-  //     phase2MinorSelection: studyRight.phase2MinorSelection,
-  //     admissionTypeUrn: studyRight.admissionTypeUrn,
-  //     educationPhase1: studyRight.educationPhase1,
-  //     educationPhase2: studyRight.educationPhase2,
-  //     createdAt: studyRight.createdAt,
-  //     updatedAt: studyRight.updatedAt,
-  //   })
-  // })
-  // await Promise.all(creations)
+  console.log('finished batch of: ', count) 
 }
 
 //assumes that there is a table Users with studentNumber field
