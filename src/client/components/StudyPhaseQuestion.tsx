f2d7cfbe132593a7ee69e21b7fc10453dd27b3bd
matiassinisalo <matias.aleksanteri@hotmail.fi>
2025-07-25 13:26:59 +0300

    fix studyfield not being selectable when there are multiple options

--- src/client/components/StudyPhaseQuestion.tsx
+++ src/client/components/StudyPhaseQuestion.tsx
@@ -1,15 +1,26 @@
 import { Box, MenuItem, Select, Typography } from '@mui/material'
+import { useState } from 'react'
 
 const StudyPhaseQuestion = ({ studyData }: { studyData: any }) => {
   const organisations = studyData?.organisations
+  const startValue = () => {   
+    if(!organisations){
+      return null
+    }
+    const selectedField = organisations[0]
+    return selectedField.code
+  }
+  const [selectedValue, setSelectedValue] = useState(startValue())
+  
+
+  const handleChange = (e) => {
+    e.preventDefault()
+    setSelectedValue(e.target.value)
+  }
+
   if(!organisations){
     return (<p>no organisation found</p>)
   }
-  const selectedField = organisations[0]
-  if(!selectedField){
-    return(<p>no field found</p>)
-  }
-
   return (
     <Box sx={{ minWidth: 200, marginBottom: 2 }}>
       
@@ -27,7 +38,8 @@ const StudyPhaseQuestion = ({ studyData }: { studyData: any }) => {
           disabled={organisations.length < 2 ? true : false} //makes drop down disabled if there is only one option to choose
           name="study-field-select"
           labelId="study-field-select-label"
-          value={selectedField.code}
+          value={selectedValue}
+          onChange={handleChange}
         >
           {organisations?.map((item: any) => (
             <MenuItem key={item.id} value={item.code}>
@@ -35,7 +47,7 @@ const StudyPhaseQuestion = ({ studyData }: { studyData: any }) => {
             </MenuItem>
           ))}
         </Select>
-        {organisations.length < 2 && <input type='hidden' value={selectedField.code} name="study-field-select"/>}     
+        {organisations.length < 2 && <input type='hidden' value={selectedValue} name="study-field-select"/>}     
       </>
       
     </Box>
