16b4284f8da5a42c2520da36255899f6cdada60f
matiassinisalo <matias.aleksanteri@hotmail.fi>
2025-08-15 14:55:02 +0300

    mocked sequelize

--- src/tests/recommender.test.ts
+++ src/tests/recommender.test.ts
@@ -1,16 +1,30 @@
-import { describe, it, expect } from 'vitest'
-import recommendCourses from '../server/util/recommender'
-import { CourseRecommendations } from '../common/types'
+import { describe, it, expect, vi } from 'vitest';
+import recommendCourses from '../server/util/recommender';
+import { CourseRecommendations } from '../common/types';
+import { authenticate } from 'passport';
+import { sequelize } from '../server/db/connection';
+
+vi.mock('../server/db/connection', () => {
+  const SequelizeMock = vi.fn().mockImplementation(() => ({
+    authenticate: vi.fn().mockResolvedValue(true),
+    sync: vi.fn().mockResolvedValue(true),
+    close: vi.fn().mockResolvedValue(true),
+  }))
+
+  return {
+    sequelize: new SequelizeMock()
+  }
+})
+
 
 describe('recommender tests', () => {
   it('passes', () => {
     const val = true
     expect(val === true)
-  })
+  });
 
   it('recommender returns an empty answer with a invalid input', async() => {
     const result: CourseRecommendations = await recommendCourses({})
-
   })
 
-})
+});
