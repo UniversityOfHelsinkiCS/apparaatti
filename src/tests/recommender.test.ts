3a356afb62c4113f3ad5945128e624b30ca8ded6
matiassinisalo <matias.aleksanteri@hotmail.fi>
2025-08-15 14:37:54 +0300

    format

--- src/tests/recommender.test.ts
+++ src/tests/recommender.test.ts
@@ -1,16 +1,16 @@
-import { describe, it, expect } from 'vitest';
-import recommendCourses from '../server/util/recommender';
-import { CourseRecommendations } from '../common/types';
+import { describe, it, expect } from 'vitest'
+import recommendCourses from '../server/util/recommender'
+import { CourseRecommendations } from '../common/types'
 
 describe('recommender tests', () => {
   it('passes', () => {
     const val = true
     expect(val === true)
-  });
+  })
 
   it('recommender returns an empty answer with a invalid input', async() => {
     const result: CourseRecommendations = await recommendCourses({})
 
   })
 
-});
+})
