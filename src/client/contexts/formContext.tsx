9d9231a0ddaeadf227e196de63bf966bc7d1e419
matiassinisalo <matias.aleksanteri@hotmail.fi>
2025-11-19 16:04:31 +0200

    tweak

--- src/client/contexts/formContext.tsx
+++ src/client/contexts/formContext.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, useContext, useState, ReactNode } from 'react'
+import { createContext, useContext, useState, ReactNode } from 'react'
 import useQuestions, { pickVariant } from '../hooks/useQuestions'
 import { Question } from '../../common/types'
 
