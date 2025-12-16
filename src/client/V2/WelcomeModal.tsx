0cba2d9469ad222fd0c79adb16456fefbaa6ca4c
matiassinisalo <matias.aleksanteri@hotmail.fi>
2025-12-16 13:01:41 +0200

    close button to the modal once the questions have been answered

--- src/client/V2/WelcomeModal.tsx
+++ src/client/V2/WelcomeModal.tsx
@@ -5,6 +5,8 @@ import PrimaryLanguageSpecificationV2 from './components/PrimaryLanguageSpecific
 import { Question } from '../../common/types'
 import RadioQuestionV2 from './components/RadioQuestionV2'
 import StudyPhaseQuestionV2 from './components/StudyPhaseQuestionV2'
+import ActionButtonV2 from './components/ActionButtonV2'
+import { useTranslation } from 'react-i18next'
 
 type Props = {
   open: boolean
@@ -30,7 +32,10 @@ const WelcomeModal: FC<Props> = ({ open, onClose }) => {
     setLanguage,
     primaryLanguage,
     setPrimaryLanguage,
+    studyField,
+    primaryLanguageSpecification,
   } = useFilterContext()
+  const { t } = useTranslation()
 
   const languageQuestion = filters.find((q) => q.id === 'lang')
   const primaryLanguageQuestion = filters.find((q) => q.id === 'primary-language')
@@ -38,6 +43,17 @@ const WelcomeModal: FC<Props> = ({ open, onClose }) => {
   const primaryLanguageSpecificationQuestion = filters.find(
     (q) => q.id === 'primary-language-specification'
   )
+
+  const checkShouldShowSpecification = () => {
+     return language === primaryLanguage
+  }
+  const shouldShowSpecification = checkShouldShowSpecification()
+
+  const allQuestionsAnswered =
+    studyField &&
+    primaryLanguage &&
+    language &&
+    (shouldShowSpecification ? primaryLanguageSpecification : true)
 
   return (
     <Modal
@@ -76,6 +92,12 @@ const WelcomeModal: FC<Props> = ({ open, onClose }) => {
           <PrimaryLanguageSpecificationV2
             question={primaryLanguageSpecificationQuestion as Question}
           />
+        )}
+
+        {allQuestionsAnswered && (
+          <Box sx={{ display: 'flex', justifyContent: 'center', marginTop: 2 }}>
+            <ActionButtonV2 onClick={onClose} text={t('question:close')} />
+          </Box>
         )}
       </Box>
     </Modal>
