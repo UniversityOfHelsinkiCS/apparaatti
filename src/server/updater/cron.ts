c0f0c4f082372d376df0639c0f25d182c7ce819b
matiassinisalo <matias.aleksanteri@hotmail.fi>
2026-01-19 12:08:20 +0200

    clearing offsets on testing updater

--- src/server/updater/cron.ts
+++ src/server/updater/cron.ts
@@ -6,7 +6,7 @@ import { run } from './index.ts'
 const setupCron = async () => {
 
   if (!inProduction) {
-    await run(false)
+    await run(true)
   } else if (UPDATER_CRON_ENABLED) {
     // await run(false)
     cron.schedule('15 3,15 * * *', run(true)) // Run updater every 12 hours
