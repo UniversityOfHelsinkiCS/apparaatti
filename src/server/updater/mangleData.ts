8c0905883d22d1586dd0a7c2c03c458651e326f5
matiassinisalo <matias.aleksanteri@hotmail.fi>
2026-01-19 12:22:28 +0200

    fix constant

--- src/server/updater/mangleData.ts
+++ src/server/updater/mangleData.ts
@@ -122,11 +122,11 @@ export const mangleData2 = async(
   logger.info(`[UPDATER] Starting to update items with url ${url}`)
   const offsetKey = `${url}-offset`
   const start = Date.now()
-  const requestStart = null
+  let requestStart = null
   const loopStart = Date.now()
 
   let offset = Number(await redis.get(offsetKey))
-  const count = 0
+  let count = 0
   let iterations = 0
   const maxIterations = 10
 
