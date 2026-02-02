a3a7274ada3fdaa96c3c78ba3f06ea0ea2e9a984
matiassinisalo <matias.aleksanteri@hotmail.fi>
2026-02-02 13:08:14 +0200

    tweak

--- src/client/hooks/useQuestions.tsx
+++ src/client/hooks/useQuestions.tsx
@@ -214,7 +214,7 @@ const useQuestions = () => {
         {
           name: 'default',
           question: previouslyDoneLangQuestion,
-          options: generateGeneralYesNoOptions(),
+          options: generateGeneralYesNoOptions(false),
         },
       ],
     },
