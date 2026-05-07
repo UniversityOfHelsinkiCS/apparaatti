7b93f0c558ee73d4c0f852c870315fa45cac45c5
MatiasSinisalo <matias.aleksanteri@hotmail.fi>
2026-05-07 15:57:28 +0300

    format

--- src/client/components/common/ActionButtonV2.tsx
+++ src/client/components/common/ActionButtonV2.tsx
@@ -15,26 +15,26 @@ const ActionButtonV2 = ({
 }: actionButtonProps) => {
   const buttonSx = visualStyle === 'course-show'
     ? {
-        marginTop: 0,
-        backgroundColor: '#cfd4da',
-        color: '#1f2933',
+      marginTop: 0,
+      backgroundColor: '#cfd4da',
+      color: '#1f2933',
+      boxShadow: 'none',
+      '&:hover': {
+        backgroundColor: '#bfc6cd',
         boxShadow: 'none',
-        '&:hover': {
-          backgroundColor: '#bfc6cd',
-          boxShadow: 'none',
-        },
-      }
+      },
+    }
     : {
-        borderColor: '#b8bec5',
-        backgroundColor: '#f2f4f6',
+      borderColor: '#b8bec5',
+      backgroundColor: '#f2f4f6',
+      color: '#1f2933',
+      marginTop: 4,
+      '&:hover': {
+        borderColor: '#a7afb7',
+        backgroundColor: '#e6eaee',
         color: '#1f2933',
-        marginTop: 4,
-        '&:hover': {
-          borderColor: '#a7afb7',
-          backgroundColor: '#e6eaee',
-          color: '#1f2933',
-        },
-      }
+      },
+    }
 
   return (
     <Button
