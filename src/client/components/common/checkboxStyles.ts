ada5f1e5a93db5a2cc41e10fa5282df479ea7e6b
akskokki <akseli.kokki@gmail.com>
2026-06-16 22:48:39 +0300

    refactor: remove dead components

--- src/client/components/common/checkboxStyles.ts
+++
@@ -1,9 +0,0 @@
-import { green } from '@mui/material/colors'
-
-export const CHECKBOX_COLOR = 'default' as const
-
-export const CHECKBOX_SX = {
-  '&.Mui-checked': {
-    color: green[500],
-  },
-}
