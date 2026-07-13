73452ed98bae9c140963076c9bdbdf2e8b48e4b3
akskokki <akseli.kokki@gmail.com>
2026-07-13 14:31:54 +0300

    fix: fix broken test config

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,12 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { configDefaults, defineConfig } from 'vitest/config'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    exclude: [...configDefaults.exclude, 'e2e/**'],
+  },
   server: {
     port: 3000,
     host: true,
